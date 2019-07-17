29eaf97fce8f1adf6883e115b53fa1ff9e05d024
chrisstephenson <cs@chrisstephenson.com>
2019-07-17 22:33:54 +0300

    Bahçe updated with flanming animated death for straying butterfly....

--- cebir/Bahçe.rkt
+++ cebir/Bahçe.rkt
@@ -1,22 +1,23 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Bahçe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(require "Teachpacks/bahce-teachpack.ss")
+(require "Teachpacks/bahce-teachpack.rkt")
 
-;; get new well, butterfly, garden pictures choose at random and set coord at random in teachpack...
-(define butterfly (bitmap "Teachpacks/teachpack-images/butterfly-02.png"))
-(define kuyu (bitmap "Teachpacks/teachpack-images/kuyu.png"))
-(define bahçe (bitmap "Teachpacks/teachpack-images/garden.png"))
+(define kelebek (scale 1 (bitmap "Teachpacks/teachpack-images/butterfly-01.png")))
+(define kuyu (bitmap "Teachpacks/teachpack-images/kuyu-01.png"))
+(define bahçe (bitmap "Teachpacks/teachpack-images/bahçe-01.png"))
 
 ;;;; Tanımların burada..bahçe boyutları, marjları
-(define bahçe-genişliği WIDTH)
-(define bahçe-yüksekliği 480)
-(define kelebek-genişliği 104)
-(define kelebek-yüksekliği 75)
-(define kuyu-genişliği 100)
-(define kuyu-yüksekliği 125)
+(define bahçe-genişliği (image-width bahçe))
+(define bahçe-yüksekliği (image-height bahçe))
+(define kelebek-genişliği (image-width kelebek))
+(define kelebek-yüksekliği (image-height kelebek))
+(define kuyu-genişliği (image-width kuyu))
+(define kuyu-yüksekliği (image-height kuyu))
 (define kuyu-x 320)
-(define kuyu-y 240)
+(define kuyu-y 200)
+(define kelebek-ilk-x 420)
+(define kelebek-ilk-y 300)
 
 
 ; sözleşme, amaç ve örneklerini unutmayınız
@@ -24,39 +25,33 @@
 
 ; bahçe-içinde-sol? : Sayı -> Mantıksal
 ; karakter bahçenin sol tarafından içinde mi?
-
 (define (bahçe-içinde-sol? x)
   true)
 
 
 ; bahçe-içinde-sağ? : Sayı -> Mantıksal
 ; karakter bahçenin sağ tarafından içinde mi?
-
 (define (bahçe-içinde-sağ? x)
   true)
 
 ; bahçe-içinde-alt? : Sayı -> Mantıksal
 ; karakter bahçenin alt tarafından içinde mi?
-
 (define (bahçe-içinde-alt? y)
   true)
 
 
 ; bahçe-içinde-üst? : Sayı -> Mantıksal
 ; karakter bahçenin üst tarafından içinde mi?
-
 (define (bahçe-içinde-üst? y)
   true)
 
 ;; bahçe-içinde-mi? : Sayı Sayı-> Mantıksal
 ;; Kelebeğin hepsi hala bahçe içinde mi?
-
 (define (bahçe-içinde-mi? x y)
   (bahçe-içinde-sol? x))
 
 ;; kuyu-dışında-mı? : Sayı Sayı -> Mantıksal
 ;; Kelebeğin hepsi kuyu dışında mı?
-
 (define (kuyu-dışında-mı? x y)
   true)
 
@@ -71,7 +66,7 @@
 
 
 
-(start güvende-mi?)
+(start güvende-mi? bahçe kuyu kelebek kuyu-x kuyu-y kelebek-ilk-x kelebek-ilk-y)
 
 
 
--- cebir/Teachpacks/bahce-teachpack.rkt
+++ cebir/Teachpacks/bahce-teachpack.rkt
@@ -1,41 +1,95 @@
 #lang scheme/gui
-; export values
-; make alarm (visual) if butterfly goes out (disappear and flash?) butterfly burns up...
+; make alarm, if butterfly goes out or over well butterfly burns up...
 (require lang/prim
          lang/posn
          "bootstrap-common.rkt"
          (except-in htdp/testing test)
          (for-syntax scheme/base))
 (provide (all-from-out "bootstrap-common.rkt"))
-(provide-higher-order-primitive start (onscreen?))
-(provide WIDTH HEIGHT)
+;(provide-higher-order-primitive start (bahçe kuyu kelebek kuyu-x kuyu-y initial-x initial-y onscreen?))
+(provide-higher-order-primitive start (onscreen? bahçe kuyu kelebek kuyu-x kuyu-y initial-x initial-y))
 
-(define WIDTH  640)
-(define HEIGHT 480)
+(define flame (list (bitmap "teachpack-images/flame_0.gif")
+                    (bitmap "teachpack-images/flame_1.gif")
+                    (bitmap "teachpack-images/flame_2.gif")
+                    (bitmap "teachpack-images/flame_3.gif")
+                    (bitmap "teachpack-images/flame_4.gif")
+                    (bitmap "teachpack-images/flame_5.gif")
+                    (bitmap "teachpack-images/flame_6.gif")
+                    (bitmap "teachpack-images/flame_7.gif")
+                    ))
 
-(define butterfly (bitmap "teachpack-images/butterfly-02.png"))
-(define kuyu (bitmap "teachpack-images/kuyu.png"))
-(define bahçe (bitmap "teachpack-images/garden.png"))
+(define-struct world [bahçe kuyu kelebek kuyu-x kuyu-y x y burn])
 
-; a make-world is a (Number Number)
-; each world has an x and y coordinate
-(define-struct world [x y])
+(define (change-world w new-x new-y)
+  (make-world  (world-bahçe w)(world-kuyu w)(world-kelebek w)(world-kuyu-x w)(world-kuyu-y w) new-x new-y(world-burn w)))
+
+(define (change-burn w new-burn)
+  (make-world  (world-bahçe w)(world-kuyu w)(world-kelebek w)(world-kuyu-x w)(world-kuyu-y w) (world-x w) (world-y w) new-burn))
 
 ;; move: World Number -> Number 
 ;; did the object move? 
 (define (move w key)
-  (cond
-    [(not (string? key)) w]
-    [(string=? key "left") 
-     (make-world (- (world-x w) 10) (world-y w))]
-    [(string=? key "right") 
-     (make-world (+ (world-x w) 10) (world-y w))]
-    [(string=? key "down") 
-     (make-world (world-x w) (- (world-y w) 10))]
-    [(string=? key "up") 
-     (make-world (world-x w) (+ (world-y w) 10))]
-    [else w]))
+  (let ((step 5))
+    (cond
+      [(not (string? key)) w]
+      [(string=? key "left") 
+       (change-world w (- (world-x w) step) (world-y w))]
+      [(string=? key "right") 
+       (change-world w (+ (world-x w) step) (world-y w))]
+      [(string=? key "down") 
+       (change-world w (world-x w) (- (world-y w) step))]
+      [(string=? key "up") 
+       (change-world w (world-x w) (+ (world-y w) step))]
+      [else w])))
 
+(define (tick w)
+  (cond
+;    ((>= (world-burn w) 0) (change-burn w (modulo (add1 (world-burn w)) (length flame)))); REPEAT FLAME
+    ((>= (world-burn w) 0) (change-burn w (min (add1 (world-burn w)) (sub1 (length flame)))))
+    (else w)))
+
+(define (kelebek-image w)
+  (cond
+    ((< (world-burn w) 0) (world-kelebek w))
+    (else (list-ref flame (world-burn w)))))
+
+(define (burn? w)
+  (let*
+      ((kbx (world-x w))
+       (kby (world-y w))
+       (kbw/2 (/ (image-width (world-kelebek w)) 2))
+       (kbh/2 (/ (image-height (world-kelebek w)) 2))
+       (kb-sağ (+ kbx kbw/2))
+       (kb-sol (- kbx kbw/2))
+       (kb-üst (+ kby kbh/2))
+       (kb-alt (- kby kbh/2))
+       (kuyu-w/2 (/ (image-width (world-kuyu w)) 2))
+       (kuyu-h/2 (/ (image-height (world-kuyu w)) 2))
+       (kuyu-x (world-kuyu-x w))
+       (kuyu-y (world-kuyu-y w))
+       (kuyu-sağ (+ kuyu-x kuyu-w/2))
+       (kuyu-sol (- kuyu-x kuyu-w/2))
+       (kuyu-üst (+ kuyu-y kuyu-h/2))
+       (kuyu-alt (- kuyu-y kuyu-h/2))
+       (ak-sağ (image-width (world-bahçe w)))
+       (ak-üst (image-height (world-bahçe w))))
+    (or
+     (not
+      (and (< kb-sağ ak-sağ)
+           (> kb-sol 0)
+           (< kb-üst ak-üst)
+           (> kb-alt 0)))
+     (not
+      (or (< kb-sağ kuyu-sol)
+          (> kb-sol kuyu-sağ)
+          (< kb-üst kuyu-alt)
+          (> kb-alt kuyu-üst))))))
+       
+(define (check-burn w)
+  (cond
+    ((and (burn? w) (< (world-burn w) 0)) (change-burn w 0))
+    (else w)))
 
 ;; ----------------------------------------------------------------------------
 ;; draw-world: World -> Image 
@@ -43,9 +97,9 @@
 (define (draw-world w)
   (let* ((draw-butterfly 
           (lambda (w scene)
-            (place-image butterfly 
+            (place-image (kelebek-image w) 
                          (world-x w) 
-                         (- HEIGHT (world-y w))
+                         (- (image-height (world-bahçe w)) (world-y w))
                          scene)))
          (draw-text 
           (lambda (w scene)
@@ -58,19 +112,23 @@
               14 'black)
              scene))))
     (draw-butterfly w 
-;                    (draw-text w (empty-scene WIDTH HEIGHT)))))
-                    (draw-text w (overlay/align "middle" "middle" kuyu bahçe)))))
+                    (draw-text w (place-image  (world-kuyu w)
+                                               (world-kuyu-x w) 
+                                               (- (image-height (world-bahçe w)) (world-kuyu-y w))
+                                               (world-bahçe w))))))
 
 
-(define (start onscreen?)
-  (let* ((onscreen?* (if (= (procedure-arity onscreen?) 2) 
-                        onscreen?
-                        (lambda (x y) (onscreen? x))))
-         (update (lambda (w k) 
-                   (if (onscreen?* (world-x (move w k)) 
-                                  (world-y (move w k))) 
-                       (move w k)
-                       w))))
-    (big-bang (make-world (+ (/ WIDTH 2) 100) (+ (/ HEIGHT 2) 100))
-              (on-draw draw-world)
-              (on-key update))))
+(define (start onscreen? bahçe kuyu kelebek kuyu-x kuyu-y initial-x initial-y)
+  (let*
+      ((onscreen?* (if (= (procedure-arity onscreen?) 2) 
+                       onscreen?
+                       (lambda (x y) (onscreen? x))))
+       (update (lambda (w k) 
+                 (if (onscreen?* (world-x (move w k)) 
+                                 (world-y (move w k))) 
+                     (check-burn (move w k))
+                     w))))
+    (big-bang (make-world bahçe kuyu kelebek kuyu-x kuyu-y initial-x initial-y -1)
+      (on-draw draw-world)
+      (on-key update)
+      (on-tick tick 0.12))))
